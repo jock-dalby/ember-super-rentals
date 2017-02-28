cae42d28c933bfb99eab8ef519d3c314a6e8220f
Jock Dalby <mail@jockdalby.com>
2017-02-28 13:58:13 +1300

    add rentals/show and rentals/index nested routes. All tests passing

--- app/routes/rentals.js
+++ app/routes/rentals.js
@@ -1,7 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model() { // ES6 shorthand method definition syntax; Same as writing model: function()
-    return this.get('store').findAll('rental');
-  }
 });
